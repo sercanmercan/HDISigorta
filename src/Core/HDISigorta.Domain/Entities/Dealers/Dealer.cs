53350e04e015f795ad3bb98b80e28e0848d2759b
Sercan Mercan <smercan1996@gmail.com>
2024-02-26 18:10:51 +0300

    Dealer entity add to domain layer

---
+++ src/Core/HDISigorta.Domain/Entities/Dealers/Dealer.cs
@@ -0,0 +1,13 @@
+﻿using HDISigorta.Domain.Entities.Common;
+using HDISigorta.Domain.Entities.Identities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HDISigorta.Domain.Entities.Dealers
+{
+    public class Dealer : BaseEntity<Guid>
+    {
+        public ICollection<AppUser> AppUsers { get; set; }
+        [ForeignKey("UserId")]
+        public Guid UserId { get; set; }
+    }
+}
