394462e52cb4f883103127188716c7a04bfd6ac3
Sercan Mercan <smercan1996@gmail.com>
2024-02-26 18:10:32 +0300

    Personnel entity add to domain layer

---
+++ src/Core/HDISigorta.Domain/Entities/Personnels/Personnel.cs
@@ -0,0 +1,15 @@
+﻿using HDISigorta.Domain.Entities.Common;
+using HDISigorta.Domain.Entities.Enums;
+using HDISigorta.Domain.Entities.Identities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HDISigorta.Domain.Entities.Personnels
+{
+    public class Personnel : BaseEntity<Guid>
+    {
+        public DepartmentEnum DepartmentType { get; set; }
+        public ICollection<AppUser> AppUsers { get; set; }
+        [ForeignKey("UserId")]
+        public Guid UserId { get; set; }
+    }
+}
