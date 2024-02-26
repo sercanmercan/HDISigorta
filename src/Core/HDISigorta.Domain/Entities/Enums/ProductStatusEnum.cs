e899c18c5b84b6c22513f3c7b6140e8b1af15f7e
Sercan Mercan <smercan1996@gmail.com>
2024-02-26 18:13:21 +0300

    Product status enum add to domain layer.

---
+++ src/Core/HDISigorta.Domain/Entities/Enums/ProductStatusEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDISigorta.Domain.Entities.Enums
+{
+    public enum ProductStatusEnum
+    {
+        [Description("satıldı")]
+        SoldOut = 1,
+
+        [Description("stokta var")]
+        InStock = 2,
+
+        [Description("tamirde")]
+        InRepair = 3
+    }
+}
