40dddcb771857c52bf104ba3e6e891d91852d7a7
Sercan Mercan <smercan1996@gmail.com>
2024-02-26 18:11:51 +0300

    Maintenance center add to domain layer

---
+++ src/Core/HDISigorta.Domain/Entities/MaintenanceCenters/MaintenanceCenter.cs
@@ -0,0 +1,12 @@
+﻿using HDISigorta.Domain.Entities.Identities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HDISigorta.Domain.Entities.MaintenanceCenters
+{
+    public class MaintenanceCenter
+    {
+        public ICollection<AppUser> AppUsers { get; set; }
+        [ForeignKey("UserId")]
+        public Guid UserId { get; set; }
+    }
+}
