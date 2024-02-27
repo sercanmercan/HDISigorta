9d38a05f506d6bea0c7dfdad735029360c9882dd
Sercan Mercan <smercan1996@gmail.com>
2024-02-27 15:34:42 +0300

    Product and ProductHistory entities add to domain layer

---
+++ src/Core/HDISigorta.Domain/Entities/Products/Product.cs
@@ -0,0 +1,40 @@
+﻿using HDISigorta.Domain.Entities.Agreements;
+using HDISigorta.Domain.Entities.Common;
+using HDISigorta.Domain.Entities.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HDISigorta.Domain.Entities.Products
+{
+    public class Product : BaseEntity<Guid>
+    {
+        public string Name { get; set; }
+        public double BuyingPrice { get; set; }
+        public double SellingPrice { get; set; }
+        public ProductStatusEnum ProductStatus { get; set; }
+        public bool IsSold { get; set; }
+
+        //Değişen parçası var mı
+        public bool IsChangedPart { get; set; }
+
+        //Tamir edilen parçası var mı
+        public bool IsRepairedPart { get; set; }
+
+        //Satış zamanı
+        public DateTime? SellingTime { get; set; }
+
+        //Alış zamanı
+        public DateTime? BuyingTime { get; set; }
+
+        //Garanti içeriği
+        public Agreement Agreement { get; set; }
+        [ForeignKey(nameof(AgreementId))]
+        public Guid AgreementId { get; set; }
+        public ICollection<ProductHistory> ProductHistories { get; set; }
+
+        //Kar marjı
+        public double ProfitMargin  { get; set; }
+
+        //Garanti risk maliyeti
+        public double RiskCost { get; set; }
+    }
+}
---
+++ src/Core/HDISigorta.Domain/Entities/Products/ProductHistory.cs
@@ -0,0 +1,14 @@
+﻿using HDISigorta.Domain.Entities.Common;
+using HDISigorta.Domain.Entities.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HDISigorta.Domain.Entities.Products
+{
+    public class ProductHistory : BaseEntity<Guid>
+    {
+        public Product Product { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public Guid ProductId { get; set; }
+        public ProductStatusEnum ProductStatus { get; set; }
+    }
+}
