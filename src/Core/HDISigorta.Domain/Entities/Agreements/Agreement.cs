36d9100f675a67f8eabdf94fae23482c8df68fd9
Sercan Mercan <smercan1996@gmail.com>
2024-02-27 15:35:45 +0300

    Agreement entity add to domain layer

---
+++ src/Core/HDISigorta.Domain/Entities/Agreements/Agreement.cs
@@ -0,0 +1,18 @@
+﻿using HDISigorta.Domain.Entities.Common;
+using HDISigorta.Domain.Entities.Products;
+
+namespace HDISigorta.Domain.Entities.Agreements
+{
+    public class Agreement : BaseEntity<Guid>
+    {
+        public string Name { get; set; }
+
+        //Garanti içeriği
+        public string Description { get; set; }
+
+        //Geçerlilik süresi
+        public int ValidityPeriod { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
