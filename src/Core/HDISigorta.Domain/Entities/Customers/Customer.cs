c7a7bf8978ef41952f7df78ea4d6417d79200afd
Sercan Mercan <smercan1996@gmail.com>
2024-02-27 15:35:10 +0300

    Customer entity add to domain layer

---
+++ src/Core/HDISigorta.Domain/Entities/Customers/Customer.cs
@@ -0,0 +1,23 @@
+﻿using HDISigorta.Domain.Entities.Common;
+using HDISigorta.Domain.Entities.Dealers;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HDISigorta.Domain.Entities.Customers
+{
+    public class Customer : BaseEntity<Guid>
+    {
+        [MaxLength(11)]
+        public string IdentityNumber { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+
+        [MaxLength(11)]
+        public string PhoneNumber { get; set; }
+
+        //Hangi bayiden satış yapıldı.
+        public Dealer Dealer { get; set; }
+        [ForeignKey(nameof(DealerId))]
+        public Guid DealerId { get; set; }
+    }
+}
