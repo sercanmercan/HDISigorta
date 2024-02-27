494d2e5b2fc843c0dc1ee74949a23a8d640bd078
Sercan Mercan <smercan1996@gmail.com>
2024-02-27 15:33:55 +0300

    Maintenance entity add to domain layer

---
+++ src/Core/HDISigorta.Domain/Entities/Maintenance/Maintenance.cs
@@ -0,0 +1,21 @@
+﻿using HDISigorta.Domain.Entities.Common;
+using HDISigorta.Domain.Entities.MaintenanceCenters;
+using HDISigorta.Domain.Entities.Products;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HDISigorta.Domain.Entities.Maintenance
+{
+    public class Maintenance : BaseEntity<Guid>
+    {
+        public Product Product { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public Guid ProductId { get; set; }
+        public DateTime EntryTime { get; set; } = DateTime.Now;
+        public DateTime? CheckOut { get; set; }
+        public string Description { get; set; }
+
+        public MaintenanceCenter MaintenanceCenter { get; set; }
+        [ForeignKey(nameof(MaintenanceCenterId))]
+        public  Guid MaintenanceCenterId { get; set; }
+    }
+}
