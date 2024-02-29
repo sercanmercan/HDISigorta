52026a2fb6c35a50e6f4d7d3a9835958d40294f7
Sercan Mercan <smercan1996@gmail.com>
2024-02-29 14:32:30 +0300

    Agreement read and write repository add to application and persistence layer

---
+++ src/Core/HDISigorta.Application/Repositories/Agreements/IAgreementWriteRepository.cs
@@ -0,0 +1,8 @@
+﻿using HDISigorta.Domain.Entities.Agreements;
+
+namespace HDISigorta.Application.Repositories.Agreements
+{
+    public interface IAgreementWriteRepository : IWriteRepository<Agreement>
+    {
+    }
+}
---
+++ src/Infrastructure/HDISigorta.Persistence/Repositories/Agreements/AgreementWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using HDISigorta.Application.Repositories.Agreements;
+using HDISigorta.Domain.Entities.Agreements;
+using HDISigorta.Persistence.Contexts;
+
+namespace HDISigorta.Persistence.Repositories.Agreements
+{
+    public class AgreementWriteRepository : WriteRepository<Agreement>, IAgreementWriteRepository
+    {
+        public AgreementWriteRepository(HDISigortaDbContext context) : base(context)
+        {
+        }
+    }
+}
